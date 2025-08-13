b5d1ed543a243d792be40d8d9189c3b98b150c99
cuixiaolong <779997748@qq.com>
2025-08-13 15:59:00 +0800

    初始化，以及用户的注册登录权限控制

---
+++ src/main/java/com/yupi/yupicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.yupicturebackend.mapper;
+
+import com.yupi.yupicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author 77999
+ * @description 针对表【user(用户)】的数据库操作Mapper
+ * @createDate 2025-08-12 21:42:25
+ * @Entity com.yupi.yupicturebackend.model.entity.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/yupicturebackend/service/UserService.java
@@ -0,0 +1,60 @@
+package com.yupi.yupicturebackend.service;
+
+import com.yupi.yupicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.yupicturebackend.model.vo.LoginUserVO;
+import com.yupi.yupicturebackend.model.vo.UserVO;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * @author 77999
+ * @description 针对表【user(用户)】的数据库操作Service
+ * @createDate 2025-08-12 21:42:25
+ */
+public interface UserService extends IService<User> {
+
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    String getEncryptPassword(String userPassword);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    UserVO getUserVO(User user);
+
+    List<UserVO> getUserVOList(List<User> userList);
+}
