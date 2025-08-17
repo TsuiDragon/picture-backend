1712405e08fd90d050155be7349e8cf9e5c81a38
cuixiaolong <779997748@qq.com>
2025-08-17 17:32:13 +0800

    批量编辑图片

--- src/main/java/com/yupi/yupicturebackend/service/PictureService.java
+++ src/main/java/com/yupi/yupicturebackend/service/PictureService.java
@@ -8,6 +8,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.yupi.yupicturebackend.model.entity.User;
 import com.yupi.yupicturebackend.model.vo.PictureVO;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -72,4 +73,7 @@ public interface PictureService extends IService<Picture> {
     void checkPictureAuth(User loginUser, Picture picture);
 
     List<PictureVO> searchPictureByColor(Long spaceId, String picColor, User loginUser);
+
+    @Transactional(rollbackFor = Exception.class)
+    void editPictureByBatch(PictureEditByBatchRequest pictureEditByBatchRequest, User loginUser);
 }
