dcf186a9ad0d355ae17cac8533c9a13ccb619839
cuixiaolong <779997748@qq.com>
2025-08-18 11:54:09 +0800

    图库分析 1.空间资源使用情况 2.空间图片分类分析 3.空间图片标签分析 4.空间图片大小分析 5.用户上传行为分析 6.空间使用排行分析

---
+++ src/main/java/com/yupi/yupicturebackend/service/SpaceAnalyzeService.java
@@ -0,0 +1,22 @@
+package com.yupi.yupicturebackend.service;
+
+import com.yupi.yupicturebackend.model.dto.space.analyze.*;
+import com.yupi.yupicturebackend.model.entity.Space;
+import com.yupi.yupicturebackend.model.entity.User;
+import com.yupi.yupicturebackend.model.vo.space.analyze.*;
+
+import java.util.List;
+
+public interface SpaceAnalyzeService {
+    SpaceUsageAnalyzeResponse getSpaceUsageAnalyze(SpaceUsageAnalyzeRequest spaceUsageAnalyzeRequest, User loginUser);
+
+    List<SpaceCategoryAnalyzeResponse> getSpaceCategoryAnalyze(SpaceCategoryAnalyzeRequest spaceCategoryAnalyzeRequest, User loginUser);
+
+    List<SpaceTagAnalyzeResponse> getSpaceTagAnalyze(SpaceTagAnalyzeRequest spaceTagAnalyzeRequest, User loginUser);
+
+    List<SpaceSizeAnalyzeResponse> getSpaceSizeAnalyze(SpaceSizeAnalyzeRequest spaceSizeAnalyzeRequest, User loginUser);
+
+    List<SpaceUserAnalyzeResponse> getSpaceUserAnalyze(SpaceUserAnalyzeRequest spaceUserAnalyzeRequest, User loginUser);
+
+    List<Space> getSpaceRankAnalyze(SpaceRankAnalyzeRequest spaceRankAnalyzeRequest, User loginUser);
+}
--- src/main/java/com/yupi/yupicturebackend/service/impl/SpaceServiceImpl.java
+++ src/main/java/com/yupi/yupicturebackend/service/impl/SpaceServiceImpl.java
@@ -191,6 +191,12 @@ public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space> implements
         return queryWrapper;
     }
 
+    @Override
+    public void checkSpaceAuth(User loginUser, Space space) {
+        if (!space.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);}
+    }
+
 
 }
 
